c7511b685b39aa2939cae3ce8e109969a9445564
Daniel Haake <47055554+danielhaake@users.noreply.github.com>
2021-02-23 13:17:04 +0100

    set gunicorn for access log to stdout (#8)

--- docker/app/run.sh
+++ docker/app/run.sh
@@ -2,4 +2,4 @@
 
 export AWS_CONTAINER_CREDENTIALS_RELATIVE_URI
 
-pipenv run gunicorn app:server -b 0.0.0.0:8000 --workers=2 --threads=4 --worker-class=gthread --log-file=-
+pipenv run gunicorn app:server -b 0.0.0.0:8000 --workers=2 --threads=4 --worker-class=gthread --log-file=- --access-logfile=-
