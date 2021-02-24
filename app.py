e8be2eebb75e759ae287f57fff452bcd658ac5d0
Daniel Haake <daniel.haake@potsdam.de>
2021-02-24 19:24:51 +0100

    add flask caching

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 import dash
 import dash_bootstrap_components as dbc
+from flask_caching import Cache
 
 from app_layout import *
 
@@ -24,11 +25,17 @@ app.layout = layout.layout()
 
 server = app.server  # important for using with gunicorn
 
+cache = Cache(app.server, config={
+    'CACHE_TYPE': 'simple'
+})
+
+timeout = 600  # seconds
 
 # Reloading graphs to get graphs with new data
 @app.callback(
     dash.dependencies.Output('tabs-global-overview', 'children'),
     [dash.dependencies.Input('graph-update', 'n_intervals')])
+@cache.memoize(timeout=timeout)
 def update_tabs_with_graphs(n):
     return layout.tabs_with_graphs()
 
