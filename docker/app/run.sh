5962115b1c5fc2a8ddc67a0a278a3f75e62c4f3e
danielhaake <daniel.haake@potsdam.de>
2021-12-18 12:54:52 +0100

    delete unneccessary things

--- docker/app/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export AWS_CONTAINER_CREDENTIALS_RELATIVE_URI
-
-pipenv run gunicorn app:server -b 0.0.0.0:8000 --workers=2 --threads=4 --worker-class=gthread --log-file=- --access-logfile=-
--- docker/update_data/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export AWS_CONTAINER_CREDENTIALS_RELATIVE_URI
-
-pipenv run python update_data.py
